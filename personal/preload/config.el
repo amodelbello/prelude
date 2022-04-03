072e66c3d02d5608a474613138a51d71bc01e90f
Amo DelBello <2342804+amodelbello@users.noreply.github.com>
2022-04-02 19:04:40 -0600

    Add paredit

--- personal/preload/config.el
+++ personal/preload/config.el
@@ -8,4 +8,19 @@
 
 (use-package format-all
   :ensure t
-  :config (format-all-mode 1))
+  :config (format-all-mode 1)
+  :init
+  (progn
+    (add-hook 'prog-mode-hook 'format-all-mode)
+    (add-hook 'format-all-mode-hook 'format-all-ensure-formatter)
+    (add-hook 'before-save-hook 'format-all-buffer)))
+
+(use-package paredit
+  :ensure t
+  :init
+  (progn
+    (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+    (add-hook 'clojure-mode-hook 'paredit-mode)
+    (add-hook 'clojurescript-mode-hook 'paredit-mode)
+    (add-hook 'clojurec-mode-hook 'paredit-mode)
+    (add-hook 'cider-repl-mode-hook 'paredit-mode)))
